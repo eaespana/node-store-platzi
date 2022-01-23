f3af08e2855b90e8cba80524e8e5a920319a9266
Alejandro España <alejo-e-1@hotmail.com>
2022-01-22 20:34:07 -0500

    Revert "Add routes customer, category and users"
    This reverts commit 71c9d2b3879763dc183b8587cc6b19660c57a748.

--- services/user.service.js
+++ services/user.service.js
@@ -1,8 +1,4 @@
 const faker = require('faker');
-const boom = require('@hapi/boom');
-
-const getConnectionBd = require('../libs/postgres');
-const {models} = require('./../libs/sequelize');
 
 class UsersService {
 
@@ -23,41 +19,24 @@ class UsersService {
     }
   }
 
-  async create(data){
-    const newUser = await models.User.create(data);
-    return newUser;
+  create(){
 
   }
 
-  async find() {
-    const rta = await models.User.findAll({
-      include: ['customer']
-    });
-    return rta;
-    /*const client = await getConnectionBd();
-    const rta = await client.query('SELECT * FROM public.tasks');
-    return rta.rows;*/
-    //return this.users;
+  find() {
+    return this.users;
   }
 
-  async findOne(id) {
-    const user = await models.User.findByPk(id);
-    if(!user){
-      throw boom.notFound('user not found');
-    }
-    return user;
+  findOne(id) {
+    return this.users.find(item => item.id === id);
   }
 
-  async update(id, changes){
-    const user = await this.findOne(id);
-    const rta = await user.update(changes);
-    return rta;
+  update(){
+
   }
 
-  async delete(id){
-    const user = await this.findOne(id);
-    await user.destroy();
-    return id;
+  delete(){
+
   }
 
 }
